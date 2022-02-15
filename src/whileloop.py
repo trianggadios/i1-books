d7b7f999bd4202d81ac8015cbeea602d1062ae1c
Tri Angga Dio Simamora <trianggadio@gmail.com>
2022-02-15 15:37:33 +0700

    initial push

---
+++ src/whileloop.py
@@ -0,0 +1,9 @@
+#Contoh penggunaan While Loop
+#Catatan: Penentuan ruang lingkup di Python bisa menggunakan tab alih-alih menggunakan tanda kurung
+
+count = 0
+while (count < 9):
+    print ("The count is: ", count)
+    count = count + 1
+
+print ("Good bye!")
